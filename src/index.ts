7805873ebd0b9267cd1aff813f6a719881805bed
Gabriel de Oliveira Estevam <gabriel.oliveira@vamosparcelar.com.br>
2022-12-23 15:33:35 -0300

    Include morgan log request http

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
+var morgan = require('morgan');
 
 // Conecção do banco de dadoss
 AppDataSource.initialize().then(() => {
     const app = express();
 
     app.use(express.json());
+
+    app.use(morgan('dev'));
 
     app.use(routes);
 
