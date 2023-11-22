28e610313a24926ff517672c401d1cd9a576a102
Gabriel de Oliveira Estevam <gabriel.oliveira@vamosparcelar.com.br>
2023-11-22 19:27:05 -0300

    adiciona husky, eslint e prettier

--- src/providers/amqpProvider.ts
+++ src/providers/amqpProvider.ts
@@ -1,10 +1,10 @@
-import { AMQPService } from '@services/rabbitMQ/amqpService';
-import consumerFactory from '@shared/factory';
+import { AMQPService } from '@services/rabbitMQ/amqpService'
+import consumerFactory from '@shared/factory'
 
 export const SetupAmqpProvider = async () => {
     try {
-        const amqpService = consumerFactory<AMQPService>('AMQPService');            
+        consumerFactory<AMQPService>('AMQPService')
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
-};
+}
--- src/services/rabbitMQ/amqpService.ts
+++ src/services/rabbitMQ/amqpService.ts
@@ -1,52 +1,56 @@
-import { Config } from '@config/index';
-import { injectable } from 'tsyringe';
-import amqp, { AmqpConnectionManager, Channel, Options } from 'amqp-connection-manager';
+import { Config } from '@config/index'
+import { injectable } from 'tsyringe'
+import amqp, {
+    AmqpConnectionManager,
+    Channel,
+    Options,
+} from 'amqp-connection-manager'
 
 @injectable()
 class AMQPService {
-    private connection: AmqpConnectionManager;
-    private channel: Channel;
+    private connection: AmqpConnectionManager
+    private channel: Channel
 
     private readonly assertExchangeOptions: Options.AssertExchange = {
         durable: true,
         autoDelete: false,
-    };
-      
-    constructor(
+    }
 
-    ){
-        this.setup();
+    constructor() {
+        this.setup()
     }
 
     private setup(): void {
-        this.connection = amqp.connect(
-          Config.RABBITMQ.URI,
-          {
+        this.connection = amqp.connect(Config.RABBITMQ.URI, {
             connectionOptions: {
-              clientProperties: {
-                connection_name: 'api-rest-typescript.api',
-              },
+                clientProperties: {
+                    connection_name: 'api-rest-typescript.api',
+                },
             },
-          },
-        );
-    
+        })
+
         this.channel = this.connection.createChannel({
             setup: async (channel: Channel) => {
-              console.log('Conectado ao RabbitMQ. Realizando configurações das exchanges e filas.');
-              await this.setupQueuesAndExchanges(channel);
+                console.log(
+                    'Conectado ao RabbitMQ. Realizando configurações das exchanges e filas.',
+                )
+                await this.setupQueuesAndExchanges(channel)
             },
-          });
-    
-      }
-    
+        })
+    }
+
     private async setupQueuesAndExchanges(channel: Channel): Promise<void> {
-        this.channel.assertExchange('exchange-student-grade', 'direct', this.assertExchangeOptions);
-        console.log('Configuração de exchange e filas finalizada');
+        channel.assertExchange(
+            'exchange-student-grade',
+            'direct',
+            this.assertExchangeOptions,
+        )
+        console.log('Configuração de exchange e filas finalizada')
     }
 
     getConnection<TConnection>(): TConnection {
-      return this.connection as unknown as TConnection;
-    }    
-};
+        return this.connection as unknown as TConnection
+    }
+}
 
-export { AMQPService }; 
+export { AMQPService }
