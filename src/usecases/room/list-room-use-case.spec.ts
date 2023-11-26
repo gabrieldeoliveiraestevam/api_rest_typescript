0b007d14cf473ac2ecb05ce78355b8497a8fc3db
Gabriel de Oliveira Estevam <gabriel.oliveira@vamosparcelar.com.br>
2023-11-26 15:23:04 -0300

    test: inclui novo teste no use case list room

--- src/usecases/room/list-room-use-case.spec.ts
+++ src/usecases/room/list-room-use-case.spec.ts
@@ -3,6 +3,7 @@ import { mock, MockProxy, mockReset } from 'jest-mock-extended';
 import { RoomRepositoryTypeOrm } from "@repositories/room-repository";
 import { Room } from "@entities/room";
 import { ListRoomUseCase } from './list-room-use-case';
+import { succes } from '@usecases/errors/either';
 
 describe('ListRoomUseCase', () => {
     let mockRoomRepositoryTypeOrm: MockProxy<RoomRepositoryTypeOrm>;
@@ -18,6 +19,15 @@ describe('ListRoomUseCase', () => {
         mockRoomRepositoryTypeOrm.find.mockReturnValue(mockResponseRoomRepository);
     } )
 
+    test('Should return array class room when correct execution', async () => {
+        
+        const sut = new ListRoomUseCase(mockRoomRepositoryTypeOrm);
+
+        const response = await sut.execute();
+
+        expect(response).toEqual(succes({list: mockResponseRoomRepository}));
+    
+    });
 
     test('Should throw an error when an error occurs in find', async () => {
         mockRoomRepositoryTypeOrm.find.mockImplementation( () => {
