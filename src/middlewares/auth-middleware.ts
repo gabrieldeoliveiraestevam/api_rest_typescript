99b7dc16d18f5d98b9d409652569586786b76669
Gabriel de Oliveira Estevam <gabriel.oliveira@vamosparcelar.com.br>
2023-12-06 11:29:59 -0300

    feat: trata exceção do middleware auth

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -9,29 +9,34 @@ type JwtPayload = {
 
 // Processo de autenticação de usuário logado => Melhorar!!!!
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const { authorization } = req.headers;
-    // Verifica se authorization existe no header
-    if(!authorization){
-        return res.status(401).json("Not authorized");
-    }
-
-    const token = authorization.split(' ')[1];
-    // Verifica o token não foi informado
-    if (token == "undefined"){
-        return res.status(401).json("Not authorized");
-    };
-
-    const { id } = jwt.verify(token, Config.JWT.PASSWORD) as JwtPayload;
+    try {
+        const { authorization } = req.headers;
+        // Verifica se authorization existe no header
+        if(!authorization){
+            return res.status(401).json("Not authorized");
+        }
     
-    const userAuth = await userRepositoryTypeOrm.findOne({ 
-        where: {id: id}
-    })
-    // Verifica se o token informado é valido. Pesquisando na tabela user.
-    if(!userAuth){
-        return res.status(401).json("Not authorized");
+        const token = authorization.split(' ')[1];
+        // Verifica o token não foi informado
+        if (token == "undefined"){
+            return res.status(401).json("Not authorized");
+        };
+    
+        const { id } = jwt.verify(token, Config.JWT.PASSWORD) as JwtPayload;
+        
+        const userAuth = await userRepositoryTypeOrm.findOne({ 
+            where: {id: id}
+        })
+        // Verifica se o token informado é valido. Pesquisando na tabela user.
+        if(!userAuth){
+            return res.status(401).json("Not authorized");
+        }
+        // Inclui o user autenticado no request
+        req.user = userAuth;
+    
+        next();       
+    } catch (error) {
+        return res.status(500).json("Not authorized");
     }
-    // Inclui o user autenticado no request
-    req.user = userAuth;
 
-    next();
 }
